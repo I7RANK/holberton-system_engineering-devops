3979841349fd01e6f953fe6e4c678233f43f2c37
I7RANK <franfgh2000@hotmail.com>
2020-12-10 12:44:27 -0500

    deleting the exec restart task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,9 +10,16 @@ file { '/var/www/html/index.html':
   content => 'Holberton School',
 }
 
-file_line { '/etc/nginx/sites-available/default':
+file_line { 'default':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _',
   line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
+}
+
+exec { 'restart':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0, 1],
+  after   => ['default'],
 }
