1784c57ac9615cf51067798f467dd075e68600c7
I7RANK <franfgh2000@hotmail.com>
2020-12-10 12:22:20 -0500

    uploading task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,36 @@
+# Installing nginx
+# puppet module install puppetlabs-stdlib
+
+exec { 'update':
+  command => 'sudo apt-get -y update',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0, 1],
+  before  => Package['nginx'],
+}
+
+package { 'nginx':
+  ensure => 'installed',
+  name   => 'nginx',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+
+file_line { '/etc/nginx/sites-available/default':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  require => Package['nginx'],
+}
+
+exec { 'restart':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0, 1],
+}
