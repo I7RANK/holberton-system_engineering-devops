1df72453ae27b8c3868e176839a65c6b4d96449e
I7RANK <franfgh2000@hotmail.com>
2020-12-21 12:13:07 -0500

    changing the url

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,7 +14,7 @@ file_line { '/etc/nginx/sites-available/default':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service { 'nginx_stop':
