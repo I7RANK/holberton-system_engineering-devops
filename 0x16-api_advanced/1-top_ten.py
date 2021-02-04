541ac1fb1944d327c85f32d56316f05b48c19bc6
root <root@I7RANK-PC.localdomain>
2021-02-04 17:27:18 -0500

    Uploading Task 1 and 2

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" this module contain top_ten function """
+
+import requests
+
+
+def top_ten(subreddit):
+    """ returns the first """
+    my_header = {'user-agent': 'i7RANK'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    rq = requests.get(url, headers=my_header, allow_redirects=False)
+
+    if rq.status_code == 200:
+        children = rq.json().get('data').get('children')
+        for i in range(len(children)):
+            print(children[i].get('data').get('title'))
+            if i == 9:
+                break
+    else:
+        print("None")
