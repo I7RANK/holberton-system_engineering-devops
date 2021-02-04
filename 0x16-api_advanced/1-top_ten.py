fef7b0bae1f3b7a04a0bdc721d30514c75ca439f
I7RANK <franfgh2000@hotmail.com>
2021-02-04 17:37:03 -0500

    adding params limit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,14 +7,18 @@ import requests
 def top_ten(subreddit):
     """ returns the first """
     my_header = {'user-agent': 'i7RANK'}
+    par = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    rq = requests.get(url, headers=my_header, allow_redirects=False)
+    rq = requests.get(
+        url,
+        headers=my_header,
+        allow_redirects=False,
+        params=par
+    )
 
     if rq.status_code == 200:
         children = rq.json().get('data').get('children')
-        for i in range(len(children)):
-            print(children[i].get('data').get('title'))
-            if i == 9:
-                break
+        for i in children:
+            print(i.get('data').get('title'))
     else:
         print("None")
